550e5244a702d91b995073ebb39fc4341af42c98
jdam97 <jonathandalvarez7@gmail.com>
2023-10-30 16:49:47 -0500

    feat: :sparkles: Agrego ejercicio 1 nFibonacci

---
+++ fibonacci.js
@@ -0,0 +1,18 @@
+//Reto 1 Nfibonacci
+let n = prompt(`Ingrese un número(n)`)
+//let m = prompt(`¿cuantos numeros de secuencia desea ver de fibonacci? (m) `)
+
+let nfibonacci = function (n,m){
+    if (n>40) { // no acepta números mayores a 40
+        return alert(`${n} No es un numero de fibonacci`)
+    }
+    else{
+        if (n<=1) {return n}
+
+        else return nfibonacci(n-1)+nfibonacci(n-2);
+        
+    }
+}
+
+alert(`Número fibonacci: ${nfibonacci(n)}`);
+
